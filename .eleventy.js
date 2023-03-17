17aaf6e1c767ba73c0f95bca70e943084a45e200
Mike Hodnick <mike@kindohm.com>
2023-03-17 13:45:45 -0500

    upgrade eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
+
+    eleventyConfig.setLiquidOptions({
+        dynamicPartials: false,
+        strictFilters: false, // renamed from `strict_filters` in Eleventy 1.0
+      });
 };
